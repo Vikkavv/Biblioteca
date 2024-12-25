de1b356a675e8acf25368df89f308f559c128246
Vikkavv <96725063+Vikkavv@users.noreply.github.com>
2024-12-25 21:41:11 +0100

    Delete Biblioteca directory

--- Biblioteca/src/main/java/Biblioteca/Controlador/BibliotecaException.java
+++
@@ -1,8 +0,0 @@
-package Biblioteca.Controlador;
-
-public class BibliotecaException extends Exception {
-    public BibliotecaException(){}
-    public BibliotecaException(String message){
-        super(message);
-    }
-}
